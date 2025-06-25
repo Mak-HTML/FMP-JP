2976450b5de9fb0426cc22420585327f412092a0
Mak-HTML <makhmoornaqi@gmail.com>
2025-06-25 12:34:33 +0500

    React-25-06-25
    React-25-06-25

---
+++ ReactJs/Assign01/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1 className="main-heading">Hello World & Hello React !!</h1>
+  </React.StrictMode>
+);
